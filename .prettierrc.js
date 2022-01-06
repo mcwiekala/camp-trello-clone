3a4edcc91b433a73914f528e4c6e540bd1e01dd1
EmilMalanczak <kdddmalanczak@gmail.com>
2022-01-06 18:53:18 +0100

    chore: init prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'always',
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  tabWidth: 2,
+  trailingComma: 'none',
+  endOfLine: 'auto'
+}
