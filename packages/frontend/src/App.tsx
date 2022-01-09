0901184bec3ae22a520235ead056077bb908510b
EmilMalanczak <kdddmalanczak@gmail.com>
2022-01-09 21:06:24 +0100

    chore: add react-router

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,8 +1,13 @@
 import { FC } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ThemeProvider } from './contexts/ThemeContext'
 
 export const App: FC = () => (
   <ThemeProvider>
-    <div>hello world</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<div>some route</div>} />
+      </Routes>
+    </BrowserRouter>
   </ThemeProvider>
 )
