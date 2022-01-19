d536532120d1ec5ccca3c87435573f8b2fc24656
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-01-19 19:14:20 +0100

    fix: 🐛 Remove MamberCard from App.tsx

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,35 +1,12 @@
-/* eslint-disable react/jsx-wrap-multilines */
 import { FC } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ThemeProvider } from './contexts/ThemeContext'
-import MemberCard from './components/MemberCard/MemberCard'
 
 export const App: FC = () => (
   <ThemeProvider>
     <BrowserRouter>
       <Routes>
-        <Route
-          path="*"
-          element={
-            <MemberCard
-              membersList={[
-                { username: 'Morris Croft', id: '1' },
-                { username: 'Kuna Hough', id: '2' },
-                { username: 'Sierran Salinas', id: '3' },
-                {
-                  username: 'Hangul Sierra',
-                  id: '4',
-                  imgUrl:
-                    'https://media.istockphoto.com/photos/orange-picture-id185284489?k=20&m=185284489&s=170667a&w=0&h=e8ZoHukToTFk2s0EqVBiZxdCs1EIKNGzT865MKU45Es='
-                },
-                { username: 'Karagana Still', id: '5' },
-                { username: 'Markus Pearson', id: '6' }
-              ]}
-              // eslint-disable-next-line no-console
-              addUserHandler={(e) => console.log(e)}
-            />
-          }
-        />
+        <Route path="*" element={<div>some route</div>} />
       </Routes>
     </BrowserRouter>
   </ThemeProvider>
