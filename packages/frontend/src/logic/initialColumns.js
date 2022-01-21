7e62869febc16627f11893ab2a9d17c40603ff31
Ryszard-S <49765395+Ryszard-S@users.noreply.github.com>
2022-01-22 00:22:12 +0100

    feat: add initial columns
    close #20

---
+++ packages/frontend/src/logic/initialColumns.js
@@ -0,0 +1,20 @@
+const InitialColumns = [
+  {
+    title: 'Backlog',
+    issues: []
+  },
+  {
+    title: 'To do',
+    issues: []
+  },
+  {
+    title: 'In progress',
+    issues: []
+  },
+  {
+    title: 'Done',
+    issues: []
+  }
+]
+
+export default InitialColumns
