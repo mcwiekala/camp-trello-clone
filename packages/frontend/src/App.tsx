777d2224278d334c7f90ac61528b1e0cb7bf818c
kacper-cyra <kacper.cyranowski@gmail.com>
2022-02-09 19:41:54 +0100

    chore: git stash pop

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { FC, useState, useMemo } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar'
 import { ThemeProvider } from './contexts/ThemeContext'
+import RandomUser from './logic/randomUser'
 import {
   DashboardsContext,
   dashboardsDefaultValue,
@@ -19,6 +21,7 @@ export const App: FC = () => {
     <ThemeProvider>
       <DashboardsContext.Provider value={memoizedDashboardsContext}>
         <BrowserRouter>
+          <Navbar loggedUser={new RandomUser().userData} boardTitle="test" />
           <Routes>
             <Route path="*" element={<div>Some routes</div>} />
           </Routes>
--- packages/frontend/src/routes/RoutesDashboards.js
+++ packages/frontend/src/routes/RoutesDashboards.js
@@ -1,10 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 import DashboardPage from '../pages/DashboardPage/DashboardPage'
 
+export const routes = {
+  dashboards: 'dashboards',
+  dashboard: 'dashboard/:id'
+}
+
 const RoutesDashboards = () => (
   <Routes>
-    <Route path="dashboards" element={<div>dashboards</div>} />
-    <Route path="dashboard/:id" element={<DashboardPage />} />
+    <Route path={routes.dashboards} element={<div>dashboards</div>} />
+    <Route path={routes.dashboard} element={<DashboardPage />} />
   </Routes>
 )
 export default RoutesDashboards
