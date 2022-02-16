98af7a3c2908dc7b93381baa304f3b47cb8e0143
Kacper Cyranowski <kacper.cyranowski@gmail.com>
2022-02-16 19:20:59 +0100

    chore: add express router

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import mongoSanitize from 'express-mongo-sanitize'
 import cors from 'cors'
+import router from './infrastructure/express/router'
 
 const app = express()
 app.use(mongoSanitize())
 
 app.use(cors())
-
+app.use('/v1', router)
 export default app
