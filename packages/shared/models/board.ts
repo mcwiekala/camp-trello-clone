db1ee0deed1095b5d0127a200321166b2e1b3c98
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-02-25 18:09:46 +0100

    feat: 🎸 Add board schema

---
+++ packages/shared/models/board.ts
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose'
+
+const boardSchema = new mongoose.Schema({
+  id: String,
+  title: String,
+  description: String,
+  createdAt: String,
+  admin: {
+    id: String,
+    username: String,
+    avatarUrl: String
+  },
+  status: String,
+  users: [
+    {
+      id: String,
+      username: String,
+      avatarUrl: String
+    }
+  ],
+  columns: [
+    {
+      id: String,
+      title: String,
+      tasks: [
+        {
+          id: String,
+          title: String,
+          captionUrl: String,
+          comments: [{ id: String }],
+          attachments: [{ id: String }],
+          users: [{ id: String }]
+        }
+      ]
+    }
+  ]
+})
+const Board = mongoose.model('User', boardSchema)
+export default Board
