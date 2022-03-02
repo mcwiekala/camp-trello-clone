e85515a0fce5868060d25bd03df25ec413d69262
Cwiekala Michal <cwiekala.michal@gmail.com>
2022-03-02 21:45:53 +0100

    chore: mongo db init

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 
+import mongoose from 'mongoose'
 import app from './app'
 import { connectToDatabase } from './infrastructure/mongoose'
 
@@ -9,8 +10,24 @@ const { PORT } = process.env
 
 let server
 
+const userSchema = new mongoose.Schema({
+  name: String
+})
+
 const startServer = async () => {
   await connectToDatabase()
+
+  console.log('init test data')
+  const User = mongoose.model('User', userSchema)
+  const mark = new User({ name: 'Mark' })
+  const adam = new User({ name: 'Adam' })
+
+  console.log(mark.name)
+  mark.save()
+  adam.save()
+
+  const docs = await User.find()
+  console.log(`Founded: ${docs.length} users with name Mark!`)
 
   server = app.listen(PORT, () => {
     console.log(`Listening to port ${PORT}`)
