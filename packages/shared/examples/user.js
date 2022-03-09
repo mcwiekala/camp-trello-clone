490b12efb97ddb2f9152b5f0eae0f1d572a6d843
Ryszard-S <49765395+Ryszard-S@users.noreply.github.com>
2022-03-09 21:14:02 +0100

    fix: data types

--- packages/shared/examples/user.js
+++ packages/shared/examples/user.js
@@ -6,11 +6,11 @@ const { ObjectId } = mongoose.Schema
 const user = new User({
   _id: ObjectId('622524b42dbfa0665c71fe8c'),
   username: 'Marek Kowalski',
-  passwordHash: 'pass123hash',
+  passwordHash: 'pass123hash1',
   avatarId: 23,
   dashboards: [
     {
-      _id: ObjectId('622524b42dbfa0665c71fe9z'),
+      _id: ObjectId('622524b42dbfa0665c71fe12'),
       imageCoverId: 'photo-1640622842924-fb0017b9d786',
       title: 'New board 1'
     },
@@ -22,8 +22,52 @@ const user = new User({
   ],
   users: [
     {
-      id: '2',
-      avatarId: 44
+      _id: ObjectId('622524b42dbfa0665c71fe9z'),
+      avatarId: 23
+    }
+  ]
+})
+
+const user2 = new User({
+  _id: ObjectId('622524b42dbfa0665c71fe9z'),
+  username: 'Jan Przykładowy',
+  passwordHash: 'pass123hash',
+  avatarId: 2,
+  dashboards: [
+    {
+      _id: ObjectId('622524b42dbfa0665c71fe9z'),
+      imageCoverId: 'photo-1640622842924-fb0017b9d786',
+      title: 'New board 1'
+    }
+  ],
+  users: [
+    {
+      _id: ObjectId('622524b42dbfa0665c71fe8c'),
+      avatarId: 414
+    }
+  ]
+})
+
+const user3 = new User({
+  _id: ObjectId('622524b42dbfa0665c71feab'),
+  username: 'Elżbieta Druga',
+  passwordHash: 'pass123hash3',
+  avatarId: 233,
+  dashboards: [
+    {
+      _id: ObjectId('622524b42dbfa0665c71feab'),
+      imageCoverId: 'photo-1640622842924-fb0017b9d723',
+      title: 'New board 4'
+    }
+  ],
+  users: [
+    {
+      _id: ObjectId('622524b42dbfa0665c71fe9z'),
+      avatarId: 23
+    },
+    {
+      _id: ObjectId('622524b42dbfa0665c71fe8c'),
+      avatarId: 2
     }
   ]
 })
