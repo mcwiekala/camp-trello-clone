5dacbcc912e5691dfa79ee3f48878a21fcb0bfc8
Ryszard-S <49765395+Ryszard-S@users.noreply.github.com>
2022-03-10 15:28:59 +0100

    fix: change photo Id

--- packages/shared/example/task.js
+++ packages/shared/example/task.js
@@ -2,8 +2,7 @@ const task1 = {
   _id: ObjectId('6225239aa76ad13dd37178b6'),
   title: 'Tytuł 1',
   description: 'Opis ',
-  imageCoverId:
-    'https://images.unsplash.com/photo-1646388023469-18941b7e3b6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  imageCoverId: 'photo-1646388023469-18941b7e3b6f',
   attachments: [
     {
       _id: ObjectId('622524c354b1fa45bdf95cd7')
