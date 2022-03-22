4e07e9fb9dbb15c5a54338aadb0eb60f4d0538ca
Cwiekala Michal <cwiekala.michal@gmail.com>
2022-03-22 19:01:37 +0100

    fix: added TODOs

--- packages/frontend/src/types/task.ts
+++ packages/frontend/src/types/task.ts
@@ -1,6 +1,7 @@
 import AttachmentType from './attachment'
 import UserType from './user'
 
+// TODO: REMOVE IT USE DTO from: packages/shared/api/dto/*.dto.ts instead
 type TaskType = {
   id: string
   imageCoverURL: string | null
