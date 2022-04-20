40033bf0c8adda36d8c36bffa43aacab1ff7f250
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-04-20 18:39:59 +0200

    fix: 🐛 Fix some eslint errors

--- packages/backend/src/infrastructure/mongoose/index.ts
+++ packages/backend/src/infrastructure/mongoose/index.ts
@@ -4,10 +4,12 @@ export const connectToDatabase = async () => {
   const { DB_USERNAME, DB_PASSWORD } = process.env
   const dbURL = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@localhost:27017/trello-clone`
 
+  /* eslint-disable no-console */
   try {
     await mongoose.connect(dbURL)
     console.log('Connected to database')
   } catch (error: unknown) {
     console.log('Error connecting to database: ', error)
   }
+  /* eslint-enable no-console */
 }
