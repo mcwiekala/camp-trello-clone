f8a4fd227464d5093c45ade2e9ac2a35f703971d
Arssin <mateusz.binieda@onet.pl>
2022-04-22 02:28:12 +0200

    feat: add delete and get attachments controllers, add routes, add postman

--- packages/backend/src/modules/attachment/attachments.controller.ts
+++ packages/backend/src/modules/attachment/attachments.controller.ts
@@ -1,3 +1,5 @@
+import fsPromises from 'fs/promises'
+import express from 'express'
 import { Attachments } from './attachments.model'
 import { fileNameHash } from '../../helpers/fileNameHash'
 
@@ -17,4 +19,29 @@ export const attachmentsUpload = async (
   } catch (error) {
     res.status(400).send((error as Error).message)
   }
+}
+
+export const findByIdAndRemove = async (req: express.Request, res: express.Response) => {
+  const { id } = req.params
+  const file = await Attachments.findById(id)
+  if (!file) {
+    return res.status(404).json({ message: 'This Attachment does not exist' })
+  }
+  try {
+    await fsPromises.unlink(`uploads/${file.fileNameHash}`)
+    await Attachments.findByIdAndRemove(id)
+  } catch (err) {
+    console.log(err)
+  }
+  return res.status(200).json({ message: 'Attachment has been successfully deleted' })
+}
+
+export const findById = async (req: express.Request, res: express.Response) => {
+  const { id } = req.params
+  const file = await Attachments.findById(id)
+  const image = `uploads/${file.fileNameHash}`
+  if (!file) {
+    return res.status(404).json({ message: 'This Attachment does not exist' })
+  }
+  return res.status(200).download(image)
 }
