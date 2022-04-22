762a131fd148f9dbfb28434ca1116080e051b6c8
Arssin <mateusz.binieda@onet.pl>
2022-04-22 02:28:12 +0200

    feat: add delete and get attachments controllers, add routes, add postman

--- packages/backend/src/modules/attachment/attachments.routes.ts
+++ packages/backend/src/modules/attachment/attachments.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express'
 import { upload } from '../../helpers/attachmentsHelper'
-import { attachmentsUpload } from './attachments.controller'
+import { attachmentsUpload, findByIdAndRemove, findById } from './attachments.controller'
 
 export const attachmentsRouter = Router()
 
 attachmentsRouter.post('/uploads', upload.single('file'), attachmentsUpload)
+
+attachmentsRouter.delete('/uploads/:id', findByIdAndRemove)
+
+attachmentsRouter.get('/uploads/:id', findById)
